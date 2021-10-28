806684879383f62ee17ed30b57984346cba55402
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-10-29 00:17:18 +0700

    finished reducer functions

--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,27 @@
-// import ACTIONS from "./config/ACTIONS";
+import ACTIONS from "./config/ACTIONS";
 
 export const reducer = (state, action) => {
   switch (action.type) {
-    
+    case ACTIONS.TOGGLE_POWER:
+      return {
+        ...state,
+        power: !state.power,
+      }
+    case ACTIONS.UPDATE_DISPLAY:
+      return {
+        ...state,
+        display: action.payload.displayText,
+      }
+    case ACTIONS.UPDATE_VOLUME:
+      return {
+        ...state,
+        volume: action.payload.volume,
+      }
+    case ACTIONS.CLEAR_DISPLAY:
+      return {
+        ...state,
+        display: '',
+      }
     default:
       return state;
   }
