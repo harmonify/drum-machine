e2ee5f623fb798f4c27df347f278fd8687b65127
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-10-29 00:20:31 +0700

    finished DisplayLabel and its state

--- src/components/DisplayLabel.jsx
+++ src/components/DisplayLabel.jsx
@@ -1,7 +1,11 @@
+import { useGlobalState } from '../hooks/useGlobalState';
+
 export function DisplayLabel() {
+  const [state] = useGlobalState();
+
   return (
     <div className="flex justify-center items-center min-w-40 min-h-8 bg-gradient-to-r from-blue-600 to-blue-500 rounded font-mono text-xl text-white" id="display">
-      Placeholder
+      {state.display}
     </div>
   )
 }
