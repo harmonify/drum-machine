5820d6974432cb62b3fd6f43c3ab2159b5c66a11
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-10-29 00:20:02 +0700

    finished DrumPad, DrumPadBank, and its states

--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -1,10 +1,46 @@
-export function DrumPad() {
+import { useGlobalState } from '../hooks/useGlobalState';
+import { useEffect, useRef } from 'react';
+import { updateDisplay } from '../actions';
+
+export function DrumPad(props) {
+  const [state, dispatch] = useGlobalState();
+  const audio = useRef(null);
+  
+  const playSound = () => {
+    if(state.power) {
+      audio.current.currentTime = 0;
+      audio.current.volume = state.volume / 100;
+      audio.current.play();
+      dispatch(updateDisplay(props.id));
+    }
+  }
+
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      if(state.power && e.keyCode === props.keyCode) {
+        playSound();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyPress);
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
+
   return (
     <button
-      className="drum-pad bg-gradient-to-l border-2 border-blue-600 text-white text-xl font-bold from-blue-600 to-blue-500 hover:from-blue-500 hover:ring rounded-lg px-6 py-4 transition duration-75"
+      className="drum-pad bg-gradient-to-l border-2 border-blue-600 text-white text-xl font-bold from-blue-600 to-blue-500 hover:from-blue-500 hover:ring focus:ring rounded-lg px-6 py-4 transition duration-75 outline-none"
+      id={props.id}
+      onClick={playSound}
     >
-      A
-      <audio src="" className="clip"></audio>
+      <audio
+        ref={audio}
+        className='clip'
+        id={props.keyTrigger}
+        src={props.url}
+      />
+      {props.keyTrigger}
     </button>
   )
 }
--- src/components/DrumPadBank.jsx
+++ src/components/DrumPadBank.jsx
@@ -1,24 +1,25 @@
 import { DrumPad } from './DrumPad';
+import BANK from '../config/BANK';
 
 export function DrumPadBank() {
+  const drumPadBank = BANK.map(el => {
+    return (
+      <DrumPad
+        key={el.id}
+        id={el.id}
+        url={el.url}
+        keyCode={el.keyCode}
+        keyTrigger={el.keyTrigger}
+      />
+    );
+  })
+  
   return (
     <div
       className="grid grid-cols-3 gap-2"
       id="drum-pad-bank"
     >
-      <DrumPad
-        text="A"
-        id="A"
-        
-      />
-      <DrumPad />
-      <DrumPad />
-      <DrumPad />
-      <DrumPad />
-      <DrumPad />
-      <DrumPad />
-      <DrumPad />
-      <DrumPad />
+      {drumPadBank}
     </div>
   )
 }
