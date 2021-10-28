133c3dae82fb66069a499c32e9a68849bfe66007
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-10-29 00:20:44 +0700

    finished VolumeRange and its state

--- src/components/VolumeRange.jsx
+++ src/components/VolumeRange.jsx
@@ -1,10 +1,31 @@
+import { useGlobalState } from '../hooks/useGlobalState';
+import { updateVolume, updateDisplay, clearDisplay } from '../actions';
+
 export function VolumeRange() {
+  const [state, dispatch] = useGlobalState();
+
+  function handleChange(e) {
+    if (state.power) {
+      dispatch(updateVolume(e.target.value));
+      dispatch(updateDisplay('Volume: ' + e.target.value));
+      setTimeout(() => dispatch(clearDisplay()), 1000);
+    }
+  }
+
   return (
     <div
       id="volume-range"
       className="transform scale-110"
     >
-      <input type="range" className="bg-blue-500 w-full" name="" id="" />
+      <input
+        type="range"
+        className="bg-blue-500 w-full"
+        id=""
+        min="0"
+        max="100"
+        value={state.volume}
+        onChange={handleChange}
+      />
     </div>
   )
 }
