b17d27af24801d28643ffc07b016473e439cbeee
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-10-29 00:19:14 +0700

    finished PowerButton and its state

--- src/components/PowerButton.jsx
+++ src/components/PowerButton.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
 import { Switch } from '@headlessui/react'
+import { clearDisplay, togglePower, updateDisplay } from '../actions';
+import { useGlobalState } from '../hooks/useGlobalState';
 
 export function PowerButton() {
-  const [enabled, setEnabled] = useState(false);
+  const [state, dispatch] = useGlobalState();
 
   function handleChange() {
-    setEnabled(! enabled);
+    dispatch(togglePower());
+    dispatch(updateDisplay('Power: ' + (!state.power ? 'ON' : 'OFF')));
+    setTimeout(() => dispatch(clearDisplay()), 1000);
   }
 
   return (
@@ -13,15 +16,15 @@ export function PowerButton() {
       <div className="flex flex-col space-y-2 justify-center items-center">
         <Switch.Label className="font-mono font-medium text-xl">Power</Switch.Label>
         <Switch
-          checked={enabled}
+          checked={state.power}
           onChange={handleChange}
           className={`${
-            enabled ? 'bg-blue-600' : 'bg-gray-200'
+            state.power ? 'bg-blue-600' : 'bg-gray-200'
           } relative inline-flex shadow-sm items-center scale-105 transform h-6 rounded-full w-11 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
         >
           <span
             className={`${
-              enabled ? 'translate-x-6' : 'translate-x-1'
+              state.power ? 'translate-x-6' : 'translate-x-1'
             } inline-block w-4 h-4 transform bg-white rounded-full transition-transform`}
           />
         </Switch>
